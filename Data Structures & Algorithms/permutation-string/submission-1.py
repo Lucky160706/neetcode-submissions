5cb056e38eb8a8a71c64d84a6ab05a71c6a91983
Quoc Phu Do <QuocPhu.Do@seattlecolleges.edu>
2026-05-31 20:46:09 -0500

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        
+        arr = list(s2)
+        
+        sorted_s1 = sorted(s1)
+
+        for i in range(len(s2) - len(s1) + 1):
+            sub_str = ''.join(arr[i : i + len(s1)])
+            if sorted(sub_str) == sorted_s1:
+                return True
+        return False
+

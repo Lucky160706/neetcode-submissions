b414c65408add4abd831d9c6fd73bbd4aedb95d3
Quoc Phu Do <QuocPhu.Do@seattlecolleges.edu>
2026-06-08 02:13:05 -0700

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        for (int i = 0; i < 2 * n; i++) {
+            if (i >= 0 && i < n) {
+                ans.push_back(nums[i]);
+            } else {
+                ans.push_back(nums[i - n]);
+            }
+        }
+        return ans;
+    }
+};

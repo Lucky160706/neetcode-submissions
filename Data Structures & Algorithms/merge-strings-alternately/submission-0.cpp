4a33d3a2425d868161ddca90b057b822d86644ab
Quoc Phu Do <QuocPhu.Do@seattlecolleges.edu>
2026-06-08 21:00:15 -0700

    Add: merge-strings-alternately - submission-0

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string ans = "";
+        int i = 0, j = 0;
+        int n = word1.size(), m = word2.size();
+        while (i < n && j < m) {
+            ans += word1[i];
+            ans += word2[j];
+            i++; j++;
+        }
+        cout << ans;
+        if (i < n) {
+            ans += word1.substr(i, n);
+        } else if (j < m) {
+            ans += word2.substr(j, m);
+        }
+        return ans;
+    }
+};

73b04630358e87f8748a758f00d35be1f55997e6
Quoc Phu Do <QuocPhu.Do@seattlecolleges.edu>
2026-06-10 15:20:50 -0700

    Add: baseball-game - submission-1

---
+++ Data Structures & Algorithms/baseball-game/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        vector<long long> ans;
+        long long total = 0;
+        int n;
+        for (int i = 0; i < operations.size(); i++) {
+            n = ans.size();
+            if (operations[i] == "+") {
+                ans.push_back(ans[n - 2] + ans[n - 1]);
+            } else if (operations[i] == "C") {
+                ans.pop_back();
+            } else if (operations[i] == "D") {
+                ans.push_back(ans[n - 1] * 2);
+            } else {
+                ans.push_back(stol(operations[i]));
+            }
+        }
+        for (long long &num : ans) {
+            total += num;
+        }
+        return total;
+    }
+};
